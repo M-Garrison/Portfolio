110ce16d14c1c72a6a34b633f1a3a2839462402f
M-Garrison <103372479+M-Garrison@users.noreply.github.com>
2022-07-28 15:45:47 -0500

    Working on my 3rd Jquery point of interaction

--- script.js
+++ script.js
@@ -17,17 +17,46 @@ $("#proj").click(function() {
     $("#projects").fadeToggle();
 });
 
+let currentImgIndex = 0
+let numOfImages = $('.family-images').children().length-1
+
+$('.next').on('click',()=>{
+    $('.family-images').children().eq(currentImgIndex).css('display','none')
+   
+    if(currentImgIndex < numOfImages) {
+        currentImgIndex ++
+       } else {currentImgIndex = 0}
+
+    $('.family-images').children().eq(currentImgIndex).css('display','block')
+})
+
+$('.previous').on('click',()=>{
+    $('.family-images').children().eq(currentImgIndex).css('display','none')
+   
+    if(currentImgIndex > 0) {
+        currentImgIndex --
+       } else {currentImgIndex = numOfImages}
+
+    $('.family-images').children().eq(currentImgIndex).css('display','block')
+})
+
 // let bg = [
 //     "images/bernard-hermant-unsplash.jpg",
 //     "images/jean-philippe-unsplash.jpg"
 // ]
-
 // $(".but1").click(function(){
 //     body.css = bg.reverse()[0];
 // });
 
+// letCurrentBG = 
+// $('.but1').on('click',()=>{
+//     $()
 
+//     $('#body').css("background-image", "url(../portfolio/images/bernard-hermant-unsplash.jpg)")
+// })
 
-
+$(".tog").click(function(){
+    $('img',this).toggle()
+})
 
 });
