ada9bfd895a6caa51ada6be2b3abad8b06318e2d
M-Garrison <103372479+M-Garrison@users.noreply.github.com>
2022-07-27 19:01:21 -0500

    the struggle continues

--- script.js
+++ script.js
@@ -1,2 +1,13 @@
 // console.log($);
 // console.log('modal practice app.js is linked to this index.html');
+$(() => {
+
+// $hero=$('<h1>').text('Testing');
+// $('#body').append($hero)
+
+
+
+
+
+
+});
