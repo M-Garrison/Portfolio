46780caa8338f69f00c0cc8e5d5222ecc954c119
M-Garrison <103372479+M-Garrison@users.noreply.github.com>
2022-07-28 12:14:09 -0500

    working on JQuery elements

--- script.js
+++ script.js
@@ -5,6 +5,26 @@ $(() => {
 // $hero=$('<h1>').text('Testing');
 // $('#body').append($hero)
 
+$("#bio").click(function() {
+    $("#biography").fadeToggle();
+});
+
+$("#res").click(function() {
+    $("#resume").fadeToggle();
+});
+
+$("#proj").click(function() {
+    $("#projects").fadeToggle();
+});
+
+// let bg = [
+//     "images/bernard-hermant-unsplash.jpg",
+//     "images/jean-philippe-unsplash.jpg"
+// ]
+
+// $(".but1").click(function(){
+//     body.css = bg.reverse()[0];
+// });
 
 
 
