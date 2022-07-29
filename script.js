df10c935deba58fe28d8b1e31764f12bbb29e3d1
M-Garrison <103372479+M-Garrison@users.noreply.github.com>
2022-07-29 12:27:28 -0500

    all 3 JQuery points of interaction complete

--- script.js
+++ script.js
@@ -2,9 +2,6 @@
 // console.log('modal practice app.js is linked to this index.html');
 $(() => {
 
-// $hero=$('<h1>').text('Testing');
-// $('#body').append($hero)
-
 $("#bio").click(function() {
     $("#biography").fadeToggle();
 });
@@ -39,34 +36,15 @@ $('.previous').on('click',()=>{
 
     $('.family-images').children().eq(currentImgIndex).css('display','block')
 });
-// attempt at the button below
-let bg = [
-    "images/bernard-hermant-unsplash.jpg",
-    "images/jean-philippe-unsplash.jpg"
-]
 
-// $(".but1").click(function(){
-//     body.css = bg.reverse()[0];
-// });
-
-$('.button').on('click',()=>{
-    $('#body').css("background-image", "url(../portfolio/images/bernard-hermant-unsplash.jpg)")
-})
-
-// $('.button').click(function(){
-//     $("body").toggleClass(function(){
-//       return "body2";
-//     });
-//   });
-
-// $('button').click(function(){
-//     $('#body').toggleClass('#body2');
-// });
-
+// change background button
+$('#button').click(function(){
+     $('.body').toggleClass('body2')
+});
 
 // toggle between photos on click
-$(".tog").click(function(){
-    $('img',this).toggle()
-});
+// $(".tog").click(function(){
+//     $('img',this).toggle()
+// });
 
 });
